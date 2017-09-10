ce1a23ee088e3295503f28ac58103747fb23be8f
Anthony <aantony4122@gmail.com>
2017-09-10 05:12:36 -0400

    fixed license header
    Signed-off-by: Anthony <aantony4122@gmail.com>

--- include/event/roomcreateevent.h
+++ include/event/roomcreateevent.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 <copyright holder> <email>
+ * Copyright 2017 Anthony Anderson aantony4122@gmail.com
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
